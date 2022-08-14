36043987db891ce6b334b80c456a1655cce05ac7
Ahmed <ahmedsaad2050@gmail.com>
2022-08-15 01:51:02 +0200

    Commit 2.0

--- cypress/integration/login_scenario/login_scenarioSD.js
+++ cypress/integration/login_scenario/login_scenarioSD.js
@@ -1,10 +1,8 @@
 import { Then } from "cypress-cucumber-preprocessor/steps"
 import LoginPage from "../../pom/LoginPage"
-import commonStepDefinitions from "../common/commonStepDefinitions"
+
 import 'cypress-mochawesome-reporter/register'
-const faker = require("faker")
-const email = commonStepDefinitions.email
-const new_password = commonStepDefinitions.new_password
+
 
 
 Then('The user should NOT be logged in with errorMSG', ()=> {
